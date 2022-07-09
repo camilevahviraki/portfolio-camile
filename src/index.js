50a63d9d9a0fc9b05ad3a484cc2a7a958118466b
camilevahviraki <camilevahviraki@gmail.com>
2022-07-09 16:25:57 +0200

    Solve dependency conflict

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 // import { Provider } from 'redux';
 // import Store from './redux/Store';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <Provider store={Store}>
-  <App />,
-  // </Provider>,
-);
+ReactDOM.render(<App />, document.getElementById('root'));
