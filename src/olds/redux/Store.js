2df129dd9d2235571b83225bbae687dcd53a1055
camilevahviraki <camilevahviraki@gmail.com>
2023-03-26 01:59:41 +0200

    Delete old components

--- src/olds/redux/Store.js
+++
@@ -1,8 +0,0 @@
-// import { createStore, combineReducers } from 'redux';
-// import popUpReducer from './popUp';
-
-// const rootReducer = combineReducers({ popUpReducer });
-
-// const Store = createStore(rootReducer);
-
-// export default Store;
