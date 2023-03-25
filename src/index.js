271eb0c457a3d2dd9f6e6e780d91dc56c9890ea3
camilevahviraki <camilevahviraki@gmail.com>
2023-03-26 01:59:15 +0200

    Update index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'redux';
-// import Store from './redux/Store';
+import { Provider } from 'react-redux';
+import Store from './redux/store';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
