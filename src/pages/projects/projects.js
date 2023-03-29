8bf17156f770ac913832da218f48b44222582963
camilevahviraki <camilevahviraki@gmail.com>
2023-03-30 00:16:30 +0200

    Update projects component

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -11,44 +11,25 @@ import './projects.css';
 const Projects = () => {
   const dispatch = useDispatch();
   const projectsLis = projectsList();
-  const [projectKey, setProjectKey] = useState(0);
+
+  const [activekey, setActiveKey] = useState(1);
+  const [startX, setStartX] = useState(0);
+  const [endX, setEndX] = useState(0);
+  const [size2L, setSize2L] = useState(80);
+  const [size3L, setSize3L] = useState(60);
+  const [size2R, setSize2R] = useState(80);
+  const [size3R, setSize3R] = useState(60);
+  const [position, setPosition] = useState('10%');
+
   const language = useSelector((state) => state.languageReducer);
   const ref1 = useRef(null);
   const ref2 = useRef(null);
-  const nextProject = () => {
-    if (projectKey + 1 < projectsLis.length) {
-      setProjectKey(projectKey + 1);
-      dispatch(resetImageShown(0));
-    }
-  };
-
-  const previousProject = () => {
-    if (projectKey - 1 >= 0) {
-      setProjectKey(projectKey - 1);
-      dispatch(resetImageShown(0));
-    }
-  };
 
   const pageInView = useSelector((state) => state.pageInViewReducer);
   const isInViewport1 = useIsInViewport(ref1);
   const isDotsInViewPort = useIsInViewport(ref2);
 
-  document.onkeydown = (e) => {
-    e = e || window.event;
-    if (e.keyCode === 39 || e.keyCode === '39') {
-      if (isDotsInViewPort) {
-        nextProject();
-      }
-    } else if (e.keyCode === 37 || e.keyCode === '37') {
-      if (isDotsInViewPort) {
-        previousProject();
-      }
-    }
-  };
-
-  const {
-    projects,
-  } = pageInView;
+  const { projects } = pageInView;
 
   if (isInViewport1 && !projects) {
     dispatch(setPageInView({
@@ -62,6 +43,99 @@ const Projects = () => {
     }));
   }
 
+  const handleNextCard = () => {
+    if (activekey < projectsLis.length) {
+      setActiveKey(activekey + 1);
+    }
+    dispatch(resetImageShown(0));
+  };
+
+  const handlePreviousCard = () => {
+    if (activekey > 1) {
+      setActiveKey(activekey - 1);
+    }
+    dispatch(resetImageShown(0));
+  };
+
+  const assignClass = (key) => {
+    if (key === activekey) {
+      return 'activeCard';
+    } if (key === activekey + 1) {
+      return 'secondActive right-card';
+    } if (key === activekey - 1) {
+      return 'secondActive left-card';
+    } if (key === activekey + 2) {
+      return 'thirdActive right-card2';
+    } if (key === activekey - 2) {
+      return 'thirdActive left-card2';
+    }
+    return 'hidden-card';
+  };
+
+  const assignSize = (key) => {
+    if (key === activekey) {
+      return 300;
+    } if (key === activekey + 1) {
+      return size2L;
+    } if (key === activekey - 1) {
+      return size2R;
+    } if (key === activekey + 2) {
+      return size3L;
+    } if (key === activekey - 2) {
+      return size3R;
+    }
+    return 0;
+  };
+
+  const handleDragStart = (event) => {
+    setStartX(event.clientX);
+  };
+
+  const handleDrag = (event) => {
+    const dragged = event.clientX - startX;
+    const absDragged = Math.sqrt(dragged * dragged);
+    if (dragged < 1 && dragged >= -50) {
+      setSize2L(80 + (40 * (absDragged / 50)));
+      setSize3L(60 + (40 * (absDragged / 50)));
+      setSize2R(80 - (40 * (absDragged / 50)));
+      setSize3R(60 - (40 * (absDragged / 100)));
+    } else if (dragged > 1 && dragged <= 50) {
+      setSize2L(80 - (40 * (absDragged / 50)));
+      setSize3L(60 - (40 * (absDragged / 50)));
+      setSize2R(80 + (40 * (absDragged / 50)));
+      setSize3R(60 + (40 * (absDragged / 50)));
+    }
+    setPosition(`calc(10% + ${dragged}px)`);
+  };
+
+  const handleDragEnd = (event) => {
+    setSize2L(80);
+    setSize3L(60);
+    setSize2R(80);
+    setSize3R(60);
+    setEndX(event.clientX);
+    setPosition('10%');
+    const distance = endX - startX;
+    if (distance > 100) {
+      handlePreviousCard();
+    } else if (distance < -100) {
+      handleNextCard();
+    }
+  };
+
+  document.onkeydown = (e) => {
+    e = e || window.event;
+    if (e.keyCode === 39 || e.keyCode === '39') {
+      if (isDotsInViewPort) {
+        handleNextCard();
+      }
+    } else if (e.keyCode === 37 || e.keyCode === '37') {
+      if (isDotsInViewPort) {
+        handlePreviousCard();
+      }
+    }
+  };
+
   return (
     <div className="projects_container" id="projects" ref={ref1}>
       <div className="project-back-blur" />
@@ -70,13 +144,46 @@ const Projects = () => {
         language.languageKey === 0 ? 'My Projects' : 'Mes Projets'
       }
       </h2>
-      <Project project={projectsLis[projectKey]} />
+      <div
+        onDragStart={handleDragStart}
+        onDrag={handleDrag}
+        onDragEnd={handleDragEnd}
+        className="project-wrapp-slider"
+      >
+        {
+          projectsLis.map((project, key) => (
+            <Project
+              project={project}
+              classComponent={assignClass(key + 1)}
+              style={activekey === key + 1
+                ? {
+                  height: `${assignSize(key + 1)}%`,
+                  left: `${position}`,
+                }
+                : { height: `${assignSize(key + 1)}%` }}
+              key={project.name}
+              languageKey={language.languageKey}
+            />
+          ))
+        }
+      </div>
+
       <div className="image-slider-buttons-wrapper">
         <div>
-          <button className="project-next-button previous-project" onClick={previousProject}>
+          <button
+            className={activekey === 1
+              ? 'project-next-button previous-project locked'
+              : 'project-next-button previous-project'}
+            onClick={handlePreviousCard}
+          >
             <FaAngleLeft />
           </button>
-          <button className="project-next-button next-project" onClick={nextProject}>
+          <button
+            className={activekey === projectsLis.length
+              ? 'project-next-button next-project locked'
+              : 'project-next-button next-project'}
+            onClick={handleNextCard}
+          >
             <FaAngleRight />
           </button>
         </div>
@@ -86,11 +193,11 @@ const Projects = () => {
         {projectsLis.map((image, id) => (
           <div
             className={
-              id === projectKey
+              id + 1 === activekey
                 ? 'image-slider-dots current-dot'
                 : 'image-slider-dots'
             }
-            onClick={() => setProjectKey(id)}
+            onClick={() => setActiveKey(id + 1)}
             key={image}
           />
         ))}
