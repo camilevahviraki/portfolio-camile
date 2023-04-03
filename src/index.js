aef09d17aaee259bfe7595fc6c5008e7ec7bcc36
camilevahviraki <camilevahviraki@gmail.com>
2023-04-03 13:21:32 +0200

    Update index.js and add react-dom-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ProjectPopUp from './pages/projects/projectPopUp';
 import Store from './redux/store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="project/popUp" element={<ProjectPopUp />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
